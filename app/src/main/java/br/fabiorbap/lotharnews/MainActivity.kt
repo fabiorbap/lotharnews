7f6d30b21299833a9d9b3d1a8a0dfba57dc4641a
Fabio Rachid <fabiorbap@gmail.com>
2025-04-30 16:34:58 -0300

    Remove unused font line

--- app/src/main/java/br/fabiorbap/lotharnews/MainActivity.kt
+++ app/src/main/java/br/fabiorbap/lotharnews/MainActivity.kt
@@ -18,7 +18,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        R.array.com_google_android_gms_fonts_certs
         enableEdgeToEdge()
         setContent {
             LotharNewsTheme {
