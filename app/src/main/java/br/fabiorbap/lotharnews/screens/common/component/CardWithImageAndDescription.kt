dfbe1a71d4c53fd1461af5f18b569d6b6b3a5818
Fabio Rachid <fabiorbap@gmail.com>
2025-05-14 17:59:50 -0300

    Adjust card UI

--- app/src/main/java/br/fabiorbap/lotharnews/screens/common/component/CardWithImageAndDescription.kt
+++ app/src/main/java/br/fabiorbap/lotharnews/screens/common/component/CardWithImageAndDescription.kt
@@ -1,17 +1,19 @@
 package br.fabiorbap.lotharnews.screens.common.component
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import br.fabiorbap.lotharnews.R
 import br.fabiorbap.lotharnews.screens.common.theme.Dimensions
@@ -26,10 +28,11 @@ fun CardWithImageAndDescription(
         modifier = modifier
             .fillMaxWidth()
             .wrapContentHeight(),
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
     ) {
         if (image != null) AsyncImage(
-            modifier = modifier.fillMaxWidth(),
+            modifier = Modifier.align(Alignment.CenterHorizontally).fillMaxSize(),
+            contentScale = ContentScale.FillBounds,
             model = image, contentDescription = stringResource(R.string.cd_card_with_image_image),
         )
         Column(modifier = modifier.padding(Dimensions.DefaultSpacing.medium)) {
